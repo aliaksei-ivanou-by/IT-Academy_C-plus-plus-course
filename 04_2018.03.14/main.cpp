29b6b282f9271dd6e9636c9d880c426b8c5ecc9e
Aliaksei Ivanou <68548528+aliaksei-ivanou-by@users.noreply.github.com>
2020-07-20 19:59:21 +0300

    04 2018.03.14

---
+++ 04_2018.03.14/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class Array
+{
+	unsigned int size;
+	int * p;
+public:
+	Array(int size_) {size = size_; p = new int[size];}
+	Array(Array &obj)
+	{
+		std::cout<<""<<std::endl;
+		std::cout<<"Copy"<<std::endl;
+		p=new int[obj.size];
+		for (int i=0; i<obj.size; i++)
+			p[i]=obj.p[i];
+		size=obj.size;
+		std::cout<<""<<std::endl;
+	}
+	int& operator[](int index)
+	{
+		if(index < size) 
+			return p[index]; 
+		else throw 5;
+	}
+	~Array(){delete [] p;}
+};
+int main()
+{	
+	// 1
+	Array arr(5);
+	for(int i = 0; i < 5; i++)
+	{
+		arr[i] = i+1;
+	}
+	try{
+		for(int i = 0; i < 6; i++)
+		{
+			std::cout << arr[i] <<std::endl;
+		}
+	}catch(int) {std::cout << "Use good!" <<std::endl;}
+	// 2
+	Array a(5);
+	for(int i = 0; i < 5; i++)
+	{
+		a[i] = i+1;
+	}
+	Array b(a);
+	for(int i = 0; i < 5; i++)
+	{
+		a[i] = i*5+1;
+	}
+	std::cout<<"a:"<<std::endl;
+	for(int i = 0; i < 5; i++)
+		{
+			std::cout << a[i] <<std::endl;
+		}
+	std::cout<<""<<std::endl;
+	std::cout<<"b:"<<std::endl;
+	for(int i = 0; i < 5; i++)
+		{
+			std::cout << b[i] <<std::endl;
+		}
+	return 0;
+}
